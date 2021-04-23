fcdbfcae87a23d3ef7753ac19196fc504882ab14
takamasa3288 <takamasa3288@icloud.com>
2021-04-23 20:37:58 +0900

    activehash編集

--- app/models/pastime.rb
+++ app/models/pastime.rb
@@ -1,15 +1,24 @@
 class Pastime < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '経済' },
-    { id: 3, name: '政治' },
-    { id: 4, name: '地域' },
-    { id: 5, name: '国際' },
-    { id: 6, name: 'IT' },
-    { id: 7, name: 'エンタメ' },
-    { id: 8, name: 'スポーツ' },
-    { id: 9, name: 'グルメ' },
-    { id: 10, name: 'その他' }
+    { id: 2, name: '映画' },
+    { id: 3, name: 'スポーツ' },
+    { id: 4, name: '音楽' },
+    { id: 5, name: 'アニメ' },
+    { id: 6, name: 'ゲーム' },
+    { id: 7, name: 'ジム・筋トレ' },
+    { id: 8, name: '旅行' },
+    { id: 9, name: '温泉' },
+    { id: 10, name: 'カフェ' },
+    { id: 11, name: 'キャンプ' },
+    { id: 12, name: '海' },
+    { id: 13, name: '料理' },
+    { id: 14, name: '食事' },
+    { id: 15, name: 'カメラ' },
+    { id: 16, name: 'ドライブ' },
+    { id: 17, name: 'スキー・スノボ' },
+    { id: 18, name: 'BBQ' },
+    { id: 19, name: 'その他' },
   ]
 
   include ActiveHash::Associations
