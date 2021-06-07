4264ad86907066d57dc534e68441a970c4780463
takamasa3288 <takamasa3288@icloud.com>
2021-06-07 20:53:56 +0900

    viewの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'tweets/index'
   root to: "tweets#index"
   devise_for :users
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
   resources :tweets, only: [:new, :create, :show, :edit, :update] do
     resources :comments, only: :create
   end
